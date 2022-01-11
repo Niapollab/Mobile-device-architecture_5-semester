97c2e16eafe92f8c5ffa5f9d8a51110ca49e5db7
Nikolay Pokataev <nikolay@iMac-Nikolay.local>
2022-01-11 05:18:43 -0800

    Initial project state

---
+++ ConcentrationProduction/ViewController.swift
@@ -0,0 +1,67 @@
+import UIKit
+
+class ViewController: UIViewController {
+    private lazy var game = Concentration(numberOfPairsOfCards: numberOfPairsOfCards)
+    
+    var emoji = [Card: String]()
+    
+    var numberOfPairsOfCards: Int {
+            return (cardButtons.count + 1) / 2
+    }
+    
+    private(set) var flipCount = 0 {
+        didSet {
+            flipCountLabel.text = "Flips: \(flipCount)"
+        }
+    }
+    
+    private(set) var emojiChoices = ["🦊", "🐷", "🐭", "🐼", "🐸", "🦁"]
+    
+    @IBAction private func touchCard(_ sender: UIButton) {
+        flipCount += 1
+        if let cardNumber = cardButtons.firstIndex(of: sender) {
+            game.chooseCard(at: cardNumber)
+            updateViewFromModel()
+        } else {
+            print("no chosen card number")
+        }
+    }
+    
+    @IBOutlet private var cardButtons: [UIButton]!
+    
+    private func updateViewFromModel() {
+        for index in cardButtons.indices {
+            let button = cardButtons[index]
+            let card = game.cards[index]
+            if card.isFaceUp {
+                button.setTitle(emoji(for: card), for: UIControl.State.normal)
+                button.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+            } else {
+                button.setTitle("", for: UIControl.State.normal)
+                button.backgroundColor = card.isMatched ? #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0) : #colorLiteral(red: 0.9529411793, green: 0.6862745285, blue: 0.1333333403, alpha: 1)
+            }
+        }
+    }
+    
+    func emoji(for card: Card) -> String {
+        if emoji[card] == nil, emojiChoices.count > 0 {
+            emoji[card] = emojiChoices.remove(at: emojiChoices.count.arc4random)
+        }
+        
+        return emoji[card] ?? "?"
+    }
+    
+    @IBOutlet private weak var flipCountLabel: UILabel!
+}
+
+extension Int {
+    var arc4random: Int {
+        if self > 0 {
+            return Int(arc4random_uniform(UInt32(self)))
+        } else if self < 0 {
+            return -Int(arc4random_uniform(UInt32(abs(self))))
+        } else {
+            return 0
+        }
+    }
+}
